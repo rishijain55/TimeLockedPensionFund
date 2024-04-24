6f9fbba86365fd0113e8cf8fb275b292626ced70
rishijain55 <rishijain55@gmail.com>
2024-04-24 12:46:25 +0530

    bugs fixed

--- contracts/TimeLock.sol
+++ contracts/TimeLock.sol
@@ -1,40 +1,125 @@
 // SPDX-License-Identifier: MIT
+// pragma solidity ^0.5.0;
+
+// contract TimeLock {
+//     struct Deposit {
+//         uint256 amount;
+//         uint256 lockUntil;
+//     }
+
+//     mapping(address => Deposit) public deposits;
+
+//     event Deposited(address indexed account, uint256 amount, uint256 lockUntil);
+//     event Redeemed(address indexed account, uint256 amount);
+
+//     function deposit() external payable {
+//         deposits[msg.sender] = Deposit(msg.value, 0);
+//         emit Deposited(msg.sender, msg.value, 0);
+//     }
+
+//     function lock(uint256 duration) external {
+//         Deposit storage deposit = deposits[msg.sender];
+//         require(deposit.amount > 0, "No deposit found");
+//         require(deposit.lockUntil == 0, "Already locked");
+
+//         deposit.lockUntil = now + duration;
+//     }
+
+//     function redeem() external {
+//         Deposit storage deposit = deposits[msg.sender];
+//         require(deposit.amount > 0, "No deposit found");
+//         require(now >= deposit.lockUntil, "Still locked");
+
+//         uint256 amount = deposit.amount;
+//         delete deposits[msg.sender];
+
+//         msg.sender.transfer(amount);
+
+//         emit Redeemed(msg.sender, amount);
+//     }
+// }
+
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.5.0;
 
 contract TimeLock {
+
+
     struct Deposit {
         uint256 amount;
         uint256 lockUntil;
     }
 
-    mapping(address => Deposit) public deposits;
+    mapping(address => Deposit[]) public deposits;
 
-    event Deposited(address indexed account, uint256 amount, uint256 lockUntil);
-    event Redeemed(address indexed account, uint256 amount);
+    event Deposited(address indexed account, uint index, uint256 amount, uint256 lockUntil);
+    event Redeemed(address indexed account, uint256 amount, uint256 depositIndex);
 
     function deposit() external payable {
-        deposits[msg.sender] = Deposit(msg.value, 0);
-        emit Deposited(msg.sender, msg.value, 0);
+        deposits[msg.sender].push(Deposit(msg.value, 0));
+        emit Deposited(msg.sender, deposits[msg.sender].length - 1, msg.value, 0);
+
     }
 
-    function lock(uint256 duration) external {
-        Deposit storage deposit = deposits[msg.sender];
-        require(deposit.amount > 0, "No deposit found");
-        require(deposit.lockUntil == 0, "Already locked");
+    function lock(uint256 depositIndex, uint256 duration) external {
+        Deposit[] storage userDeposits = deposits[msg.sender];
+        require(depositIndex < userDeposits.length, "Invalid deposit index");
+        require(userDeposits[depositIndex].lockUntil == 0, "Already locked");
 
-        deposit.lockUntil = now + duration;
+        userDeposits[depositIndex].lockUntil = now + duration;
     }
 
-    function redeem() external {
-        Deposit storage deposit = deposits[msg.sender];
+    // function redeem(uint256 depositIndex) external {
+    //     Deposit[] storage userDeposits = deposits[msg.sender];
+    //     require(depositIndex < userDeposits.length, "Invalid deposit index");
+    //     Deposit storage deposit = userDeposits[depositIndex];
+    //     require(deposit.amount > 0, "No deposit found");
+    //     require(now >= deposit.lockUntil, "Still locked");
+
+    //     uint256 amount = deposit.amount;
+    //     delete userDeposits[depositIndex];
+
+    //     msg.sender.transfer(amount);
+
+    //     //delete the deposit
+        
+
+    //     emit Redeemed(msg.sender, amount, depositIndex);
+    // }
+
+    function redeem(uint256 depositIndex) external {
+        Deposit[] storage userDeposits = deposits[msg.sender];
+        require(depositIndex < userDeposits.length, "Invalid deposit index");
+        Deposit storage deposit = userDeposits[depositIndex];
         require(deposit.amount > 0, "No deposit found");
         require(now >= deposit.lockUntil, "Still locked");
 
         uint256 amount = deposit.amount;
-        delete deposits[msg.sender];
+
+        // Remove the deposit (Shift Later Elements)
+        for (uint i = depositIndex; i < userDeposits.length - 1; i++) {
+            userDeposits[i] = userDeposits[i + 1];
+        }
+
+        // Delete the last element (now a duplicate)
+        userDeposits.pop();  
 
         msg.sender.transfer(amount);
 
-        emit Redeemed(msg.sender, amount);
+        emit Redeemed(msg.sender, amount, depositIndex);
+    }
+
+    function getDepositLength(address account) public view returns (uint256) {
+        return deposits[account].length;
+    }
+
+    function getDepositAmount(address account, uint256 index) public view returns (uint256) {
+        Deposit storage deposit = deposits[account][index];
+        return deposit.amount;
+    }
+
+    function getDepositLockUntil(address account, uint256 index) public view returns (uint256) {
+        Deposit storage deposit = deposits[account][index];
+        return deposit.lockUntil;
     }
 }
--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ const contractABI = [
       {
         "name": "",
         "type": "address"
+      },
+      {
+        "name": "",
+        "type": "uint256"
       }
     ],
     "name": "deposits",
@@ -22,7 +26,7 @@ const contractABI = [
     "payable": false,
     "stateMutability": "view",
     "type": "function",
-    "signature": "0xfc7e286d"
+    "signature": "0xd6d68177"
   },
   {
     "anonymous": false,
@@ -31,6 +35,11 @@ const contractABI = [
         "indexed": true,
         "name": "account",
         "type": "address"
+      },
+      {
+        "indexed": false,
+        "name": "index",
+        "type": "uint256"
       },
       {
         "indexed": false,
@@ -45,7 +54,7 @@ const contractABI = [
     ],
     "name": "Deposited",
     "type": "event",
-    "signature": "0x73a19dd210f1a7f902193214c0ee91dd35ee5b4d920cba8d519eca65a7b488ca"
+    "signature": "0x91ede45f04a37a7c170f5c1207df3b6bc748dc1e04ad5e917a241d0f52feada3"
   },
   {
     "anonymous": false,
@@ -59,11 +68,16 @@ const contractABI = [
         "indexed": false,
         "name": "amount",
         "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "name": "depositIndex",
+        "type": "uint256"
       }
     ],
     "name": "Redeemed",
     "type": "event",
-    "signature": "0x4896181ff8f4543cc00db9fe9b6fb7e6f032b7eb772c72ab1ec1b4d2e03b9369"
+    "signature": "0xf3a670cd3af7d64b488926880889d08a8585a138ff455227af6737339a1ec262"
   },
   {
     "constant": false,
@@ -79,6 +93,10 @@ const contractABI = [
     "constant": false,
     "inputs": [
       {
+        "name": "depositIndex",
+        "type": "uint256"
+      },
+      {
         "name": "duration",
         "type": "uint256"
       }
@@ -88,41 +106,122 @@ const contractABI = [
     "payable": false,
     "stateMutability": "nonpayable",
     "type": "function",
-    "signature": "0xdd467064"
+    "signature": "0x1338736f"
   },
   {
     "constant": false,
-    "inputs": [],
+    "inputs": [
+      {
+        "name": "depositIndex",
+        "type": "uint256"
+      }
+    ],
     "name": "redeem",
     "outputs": [],
     "payable": false,
     "stateMutability": "nonpayable",
     "type": "function",
-    "signature": "0xbe040fb0"
+    "signature": "0xdb006a75"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "account",
+        "type": "address"
+      }
+    ],
+    "name": "getDepositLength",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function",
+    "signature": "0x87ac828a"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "account",
+        "type": "address"
+      },
+      {
+        "name": "index",
+        "type": "uint256"
+      }
+    ],
+    "name": "getDepositAmount",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function",
+    "signature": "0xd02bf22f"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "account",
+        "type": "address"
+      },
+      {
+        "name": "index",
+        "type": "uint256"
+      }
+    ],
+    "name": "getDepositLockUntil",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function",
+    "signature": "0xa60bae6e"
   }
 ];
 
+
 let contract;
 let accounts;
-
 let web3;
 
 // Connect to Ethereum node
 async function connectToEthereum() {
-  if (typeof window.ethereum !== 'undefined') {
-      web3 = new Web3(window.ethereum);
-      try {
-          await window.ethereum.request({ method: 'eth_requestAccounts' });
-      } catch (error) {
-          console.error('User denied account access');
-      }
-  } else {
-      alert('Please install MetaMask to use this dApp!');
-  }
+    if (typeof window.ethereum !== 'undefined') {
+        web3 = new Web3(window.ethereum);
+        try {
+            await window.ethereum.request({ method: 'eth_requestAccounts' });
+        } catch (error) {
+            console.error('User denied account access');
+        }
+    } else {
+        alert('Please install MetaMask to use this dApp!');
+    }
 }
 
 connectToEthereum();
 
+
+async function connectContract() {
+    
+  const contractAddress = "0xD42d15690793e28d17AC0646ff2AFeB3E97070d8";
+  contract = new web3.eth.Contract(contractABI, contractAddress);
+}
+
+connectContract();
 
 // Get DOM elements
 const connectWalletBtn = document.getElementById("connectWallet");
@@ -131,19 +230,20 @@ const depositAmountInput = document.getElementById("depositAmount");
 const lockDurationInput = document.getElementById("lockDuration");
 const depositBtn = document.getElementById("deposit");
 const depositInfoDiv = document.getElementById("depositInfo");
-const redeemBtn = document.getElementById("redeem");
-const redeemInfoDiv = document.getElementById("redeemInfo");
+const depositsContainer = document.getElementById("depositsContainer");
+const currentTimeDiv = document.getElementById("currentTime");
+
 
 // Event listeners
 connectWalletBtn.addEventListener("click", connectWallet);
 depositBtn.addEventListener("click", deposit);
-redeemBtn.addEventListener("click", redeem);
 
 // Connect wallet function
 async function connectWallet() {
     try {
         accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
         walletInfoDiv.innerHTML = `Connected wallet: ${accounts[0]}`;
+        displayDeposits();
     } catch (error) {
         console.error(error);
         walletInfoDiv.innerHTML = "Error connecting wallet";
@@ -152,35 +252,101 @@ async function connectWallet() {
 
 // Deposit function
 async function deposit() {
-  if (!accounts || accounts.length === 0) {
-      alert("Please connect your wallet first!");
-      return;
-  }
+    if (!accounts || accounts.length === 0) {
+        alert("Please connect your wallet first!");
+        return;
+    }
 
-  const contractAddress = prompt("Enter contract address:");
-  contract = new web3.eth.Contract(contractABI, contractAddress);
 
-  const depositAmount = web3.utils.toWei(depositAmountInput.value, "ether");
-  const lockDuration = lockDurationInput.value;
+    const depositAmount = web3.utils.toWei(depositAmountInput.value, "ether");
+    const lockDuration = lockDurationInput.value;
 
-  try {
-      const result = await contract.methods
-          .deposit()
-          .send({ from: accounts[0], value: depositAmount });
+    try {
+        const result = await contract.methods
+            .deposit()
+            .send({ from: accounts[0], value: depositAmount });
 
-      await contract.methods
-          .lock(lockDuration)
-          .send({ from: accounts[0] });
 
-      depositInfoDiv.innerHTML = `Deposited ${web3.utils.fromWei(depositAmount, "ether")} ETH and locked for ${lockDuration} seconds`;
-  } catch (error) {
-      console.error(error);
-      depositInfoDiv.innerHTML = "Error depositing or locking";
-  }
+        //depositIndex must be length - 1
+        const depositLength = await contract.methods.getDepositLength(accounts[0]).call();
+        const depositIndex = depositLength - 1;
+        console.log("lockDuration: ", lockDuration, "depositIndex: ", depositIndex);
+
+        //lock the deposit
+        await contract.methods
+        .lock( depositIndex, lockDuration)
+        .send({ from: accounts[0] });
+        console.log("Locked deposit");
+
+        // depositInfoDiv.innerHTML = `Deposited ${web3.utils.fromWei(depositAmount, "ether")} ETH at index ${depositIndex}`;
+        displayDeposits();
+    } catch (error) {
+        console.error(error);
+        depositInfoDiv.innerHTML = "Error depositing";
+    }
+}
+
+// Display deposits function
+async function displayDeposits() {
+    console.log("Displaying deposits")
+    if (!accounts || accounts.length === 0) {
+        alert("Please connect your wallet first!");
+        return;
+    }
+
+    if (!contract) {
+        alert("Please deploy the contract first!");
+        return;
+    }
+
+    depositsContainer.innerHTML = ""; // Clear previous deposits
+
+    try {
+        const userDepositsLength = await contract.methods.getDepositLength(accounts[0]).call();
+        if (userDepositsLength === 0) {
+            depositsContainer.innerHTML = "No deposits found.";
+            console.log("No deposits found")
+            return;
+        }
+        console.log("userDepositsLength: ", userDepositsLength);
+
+
+        const depositsList = document.createElement("ul");
+
+        for (let i = 0; i < userDepositsLength; i++) {
+            //get both amount and lockUntil
+            
+            const depositAmount = await contract.methods.getDepositAmount(accounts[0], i).call();
+            const depositLockUntil = await contract.methods.getDepositLockUntil(accounts[0], i).call();
+
+
+            console.log("lock is deposited: ", depositLockUntil, "amount: ", depositAmount)
+
+            const depositItem = document.createElement("li");
+            depositItem.textContent = `Deposit ${i}: Amount: ${web3.utils.fromWei(depositAmount, "ether")} ETH, Unlock Time: ${new Date(depositLockUntil * 1000).toLocaleString()}`;
+
+
+
+
+            const redeemBtn = document.createElement("button");
+            redeemBtn.textContent = "Redeem";
+            redeemBtn.addEventListener("click", () => redeem(i));
+
+            depositItem.appendChild(document.createTextNode(" "));
+            depositItem.appendChild(redeemBtn);
+
+            depositsList.appendChild(depositItem);
+        }
+
+        depositsContainer.appendChild(depositsList);
+    } catch (error) {
+        console.error(error);
+        depositsContainer.innerHTML = "Error retrieving deposits";
+    }
 }
 
 // Redeem function
-async function redeem() {
+async function redeem(depositIndex) {
     if (!accounts || accounts.length === 0) {
         alert("Please connect your wallet first!");
         return;
@@ -191,15 +357,37 @@ async function redeem() {
         return;
     }
 
+    //get the lockUntil time
+    const lockUntil = await contract.methods.getDepositLockUntil(accounts[0], depositIndex).call();
+    //check if the lock time has passed
+    if (lockUntil > Math.floor(Date.now() / 1000)) {
+        console.log("current time: ", Math.floor(Date.now() / 1000), "lockUntil: ", lockUntil, "depositIndex: ", depositIndex);
+        alert("This deposit is still locked!");
+        console.log("This deposit is still locked!")
+        return;
+    }
+
+
     try {
         const result = await contract.methods
-            .redeem()
+            .redeem(depositIndex)
             .send({ from: accounts[0] });
 
         const amount = result.events.Redeemed.returnValues.amount;
-        redeemInfoDiv.innerHTML = `Redeemed ${web3.utils.fromWei(amount, "ether")} ETH`;
+        console.log(`Redeemed ${web3.utils.fromWei(amount, "ether")} ETH`);
+        displayDeposits();
     } catch (error) {
         console.error(error);
-        redeemInfoDiv.innerHTML = "Error redeeming";
+        console.log("Error redeeming");
     }
 }
+
+async function displayCurrentTime() {
+    const currentTimeString  = new Date().toLocaleString();
+    // console.log("Current time: ", currentTimeString);
+    currentTimeDiv.innerHTML = `Current time: ${currentTimeString}`;
+    setTimeout(displayCurrentTime, 1000);
+
+}
+
+displayCurrentTime();
